c22ea53b4111885af2a1e4bcf83147428908080c
Shaz <card@protonmail.com>
2021-03-28 00:22:24 +0100

    3 new methods

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Repo that scrapes discovery data from TikTok!
 
 This repo scrapes TikTok's Web Discovery API every 1 hour and makes commit whenever it detects any change.
-It scrapes data from "t.tiktok.com", there are 2 more domains which show different data. I'll add it on request.
+It scrapes data from "t.tiktok.com", "www.tiktok.com" and "m.tiktok.com". All of them show different trending data, not sure why. From my experience "t.tiktok.com" is the best and most reliable endpoint.
 
 Inspired by antiops' TikTok Trending Data (https://github.com/antiops/tiktok-trending-data).
 
@@ -12,9 +12,3 @@ Inspired by antiops' TikTok Trending Data (https://github.com/antiops/tiktok-tre
 * Avatars never expire! (TikTok made user avatars expire after 24 hours)
 * Nice JSON formatting
 * Shows trending users, hashtags and songs!
-
-# Usage
-Feel free to use this API without providing any credit.
-
-Currently used by:
-* https://tiktokcounter.com/
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,64 +3,70 @@ const fetch = require("node-fetch")
 
 const convertAvatar = require("./convertAvatar");
 
+var typeList = ["m", "t", "www"]
+
 module.exports = async () => {
-    const data = await fetch("https://tiktok-discovery-data.glitch.me/").then(res => res.json())
 
-    let tempArray = {
-        user: [],
-        hashtag: [],
-        music: []
-    };
+    typeList.forEach(async type => {
+        const data = await fetch(`https://tiktok-trending-data.glitch.me/${type}`).then(res => res.json())
 
-    data.body[0].exploreList.forEach(user => {
-        user = user.cardItem
-        user.subTitle = user.subTitle
-
-        tempArray.user.push({
-            userId: user.id,
-            secUserId: user.extraInfo.secUid,
-            id: user.subTitle,
-            name: user.title,
-            bio: user.description,
-            verified: user.extraInfo.verified,
-            avatar: convertAvatar(user.cover),
-            stats: {
-                followers: parseInt(user.extraInfo.fans),
-                likes: parseInt(user.extraInfo.likes)
-            }
+        let tempArray = {
+            user: [],
+            hashtag: [],
+            music: []
+        };
+    
+        data.body[0].exploreList.forEach(user => {
+            user = user.cardItem
+            user.subTitle = user.subTitle
+    
+            tempArray.user.push({
+                userId: user.id,
+                secUserId: user.extraInfo.secUid,
+                id: user.subTitle,
+                name: user.title,
+                bio: user.description,
+                verified: user.extraInfo.verified,
+                avatar: convertAvatar(user.cover),
+                stats: {
+                    followers: parseInt(user.extraInfo.fans),
+                    likes: parseInt(user.extraInfo.likes)
+                }
+            })
         })
+    
+        data.body[1].exploreList.forEach(tag => {
+            tag = tag.cardItem
+    
+            tempArray.hashtag.push({
+                challengeId: tag.id,
+                description: tag.description,
+                name: tag.title,
+                avatar: tag.cover,
+                stats: {
+                    views: parseInt(tag.extraInfo.views)
+                }
+            })
+        })
+    
+        data.body[2].exploreList.forEach(music => {
+            music = music.cardItem
+    
+            tempArray.music.push({
+                musicId: music.id,
+                avatar: convertAvatar(music.cover),
+                musicInfo: {
+                    author: music.description,
+                    title: music.title,
+                    playUrl: music.extraInfo.playUrl[0]
+                },
+                stats: {
+                    posts: music.extraInfo.posts
+                }
+            })
+        })
+    
+        return fs.writeFileSync(`../${type}.json`, JSON.stringify(tempArray, null, 2), tempArray)
     })
 
-    data.body[1].exploreList.forEach(tag => {
-        tag = tag.cardItem
-
-        tempArray.hashtag.push({
-            challengeId: tag.id,
-            description: tag.description,
-            name: tag.title,
-            avatar: tag.cover,
-            stats: {
-                views: parseInt(tag.extraInfo.views)
-            }
-        })
-    })
-
-    data.body[2].exploreList.forEach(music => {
-        music = music.cardItem
-
-        tempArray.music.push({
-            musicId: music.id,
-            avatar: convertAvatar(music.cover),
-            musicInfo: {
-                author: music.description,
-                title: music.title,
-                playUrl: music.extraInfo.playUrl[0]
-            },
-            stats: {
-                posts: music.extraInfo.posts
-            }
-        })
-    })
-
-    return fs.writeFileSync("../discover.json", JSON.stringify(tempArray, null, 2), tempArray)
 }
